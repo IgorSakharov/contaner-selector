d2acfe27b6dd042ab41bd387a333559148175eb7
Ihor Sakharov <ihor.sakharov@pixieset.com>
2025-07-21 12:05:07 -0700

    Update homebrew formula

--- homebrew-formula/container-selector.rb
+++ homebrew-formula/container-selector.rb
@@ -2,7 +2,7 @@ class ContainerSelector < Formula
   desc "Interactive Docker container selector and command executor"
   homepage "https://github.com/IgorSakharov/container-selector"  # Fixed typo
   url "https://github.com/IgorSakharov/container-selector/archive/refs/tags/v1.0.0.tar.gz"  # Fixed typo
-  sha256 "a396b37095023498a4e4ff36006dd5d0c6ce72af947a228dc90e727fbbc384df"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "go" => :build
